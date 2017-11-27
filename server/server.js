d472b856ebf78051eecb1231aea78e8bca8f24c9
edvaldatli <eddi1122@hotmail.com>
2017-11-27 21:12:10 +0000

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -21,7 +21,13 @@ app.post('/todos', (req, res) => {
   })
 });
 
-
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (error) => {
+    res.status(400).send(error);
+  });
+});
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
