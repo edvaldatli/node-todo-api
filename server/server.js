738668cd7a1530aa01da7f0eeda6a8dd1312f059
edvaldatli <eddi1122@hotmail.com>
2017-11-28 17:19:54 +0000

    Added DELETE /notes/:id test route

--- server/server.js
+++ server/server.js
@@ -69,7 +69,7 @@ app.delete('/todos/:id', (req, res) => {
       });
     }
 
-    return res.status(200).send(todo)
+    return res.status(200).send({todo})
   }).catch((error) => {
     return res.status(400).send();
   });
